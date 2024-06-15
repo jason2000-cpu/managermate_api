44d485ea80f3a6dde4199b7abe424f7953c92a46
jason2000-cpu <jasonmwangi2000@gmail.com>
2024-06-15 19:15:09 +0300

    initial commit

---
+++ api/server.js
@@ -0,0 +1,38 @@
+// const jsonServer = require('json-server')
+// const cors = require('cors')
+// const path = require('path')
+
+// const server = jsonServer.create()
+// const router = jsonServer.router(path.join(__dirname, 'db.json'))
+// const middlewares = jsonServer.defaults()
+
+// server.use(cors())
+// server.use(jsonServer.bodyParser)
+// server.use(middlewares)
+// server.use(router)
+
+// const PORT = 3001
+
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running on http://localhost:${PORT}`)
+// })
+
+
+const jsonServer = require('json-server');
+const cors = require('cors');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = server;
